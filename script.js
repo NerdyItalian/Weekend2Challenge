22dcdd292e31194c7e8906e802c65636302924d2
NerdyItalian <atava8384@gmail.com>
2015-04-27 10:22:39 -0500

    in class changes

--- script.js
+++ script.js
@@ -1,22 +1,82 @@
-var people = ["Erik", "Aaron", "Alicia", "Casie", "Clare", "Cody", "Jeanne", "Kaitlin", "Kelly", "Luke", "Mary", "Michael", "Michelle", "Rom", "Steve", "Terry", "Tracy", "Vince", "Brian", "Chelsea"];﻿
-var count;
-var peopleArray= [];
-var generatedTeam = 0;
+var teamList = [
+				"Cody",
+				"Mary",
+				"Brian",
+				"Alicia",
+				"Erik",
+				"Michael",
+				"Kelly",
+				"Chelsea",
+				"Luke",
+				"Clare",
+				"KC",
+				"Kaitlin",
+				"Michelle",
+				"Tracy",
+				"Aaron",
+				"Terry",
+				"Vince",
+				"Rom",
+				"Steve",
+				"Jeanne"
+				];
+
+var groupSize = 0;
 
 
-$(document).ready(function(){
-	$(".btn").click(function(){
-		$(this).toggleClass('selected');
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+$(document).ready(function (){
+	$(".teamSizeButton").on("click", function(){
+		groupSize = $(this).data("teamsize");
+
 	});
 
+	$(".refreshButton").on("click", function(){
+		var count = 1;
 
+		teamList = shuffle(teamList);
+
+		$(".teamContainer").empty();
+
+		for(var i = 0; i < groupSize; i++){
+			$(".teamContainer").append("<div class='nameList' id='group" + (i+1) + "'>Group " + (i+1) + "</div>");
+		}
+
+		var animationArray = [];
+
+		for(i = 0; i < teamList.length; i++){
+			var targetParent = $('#group' + count);
+			var el = "<p>" + teamList[i] + "</p>";
+			
+			targetParent.append(el);
+			el = targetParent.children().last();
+
+			el.hide().delay(1000).slideDown();
+			
+
+			count++;
+			if(count > groupSize){
+				count = 1;
+			}
+		}
+
+	});
 });
-
-
-
-
-
-
-
-function randomNumber(min, max) {
-	return Math.floor(Math.random() * (1 + max - min) + min);
